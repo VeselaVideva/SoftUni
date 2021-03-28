358560880ae2844deb7fe1d54bf883358318b20e
Vesela Videva <vesela.videva@gmail.com>
2021-03-28 21:26:58 +0300

    Add files via upload

---
+++ 2021.01 JavaScript Advanced/12. DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -0,0 +1,13 @@
+function addItem() {
+    let text = document.getElementById('newItemText').value;
+    let value = document.getElementById('newItemValue').value;
+
+    let option = document.createElement('option');
+    option.setAttribute('value', value);
+    option.textContent = text;
+
+    document.getElementById('menu').appendChild(option);
+
+    document.getElementById('newItemText').value = '';
+    document.getElementById('newItemValue').value = '';
+}
